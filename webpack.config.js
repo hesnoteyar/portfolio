0f130ad8139b478e23620915d06995fd8ad5c6e0
Jayar <jrsaranza92@gmail.com>
2025-12-15 18:33:52 +0800

    Move copy-webpack-plugin to dependencies and improve webpack config for Vercel

--- webpack.config.js
+++ webpack.config.js
@@ -44,7 +44,11 @@ module.exports = (env, argv) => {
     }),
     new CopyWebpackPlugin({
       patterns: [
-        { from: 'public', to: '.' }
+        { 
+          from: path.resolve(__dirname, 'public'), 
+          to: path.resolve(__dirname, 'dist'),
+          noErrorOnMissing: false
+        }
       ]
     }),
     ...(isProduction ? [
